dcce6b75711a223828dfc35e29b87c1fb7a2b91d
anibrahim <anibrahim@thiqah.sa>
2022-03-20 13:53:11 +0300

    initial commit add colors files update theme

---
+++ to_do_app_flutter/lib/base/AppColor.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const int greyPlatinum = 0xffe5e4e2; //titanium grey
+  static const int white = 0xffffffff; //white
+  static const Color colorAccent = Colors.pinkAccent; //white
+
+  static const Map<int, Color> greyPlatinumMap = {
+    50: Color.fromRGBO(229, 228, 226, .1),
+    100: Color.fromRGBO(229, 228, 226, .2),
+    200: Color.fromRGBO(229, 228, 226, .3),
+    300: Color.fromRGBO(229, 228, 226, .4),
+    400: Color.fromRGBO(229, 228, 226, .5),
+    500: Color.fromRGBO(229, 228, 226, .6),
+    600: Color.fromRGBO(229, 228, 226, .7),
+    700: Color.fromRGBO(229, 228, 226, .8),
+    800: Color.fromRGBO(229, 228, 226, .9),
+    900: Color.fromRGBO(229, 228, 226, 1),
+  };
+
+  static const Map<int, Color> whiteMap = {
+    50: Color.fromRGBO(255, 255, 255, .1),
+    100: Color.fromRGBO(255, 255, 255, .2),
+    200: Color.fromRGBO(255, 255, 255, .3),
+    300: Color.fromRGBO(255, 255, 255, .4),
+    400: Color.fromRGBO(255, 255, 255, .5),
+    500: Color.fromRGBO(255, 255, 255, .6),
+    600: Color.fromRGBO(255, 255, 255, .7),
+    700: Color.fromRGBO(255, 255, 255, .8),
+    800: Color.fromRGBO(255, 255, 255, .9),
+    900: Color.fromRGBO(255, 255, 255, 1),
+  };
+}
