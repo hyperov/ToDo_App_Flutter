035f47dcc682c123970ac94622f47b38e2cf77a4
creativeapps <creativeapps2020@gmail.com>
2022-04-09 07:11:36 +0200

    update test fields

--- to_do_app_flutter/lib/notes/view/NotesDetailsScreen.dart
+++ to_do_app_flutter/lib/notes/view/NotesDetailsScreen.dart
@@ -11,95 +11,131 @@ class NotesDetailsWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(() => Scaffold(
-            backgroundColor: _getBackgroundColor(
-                detailsController.bottomNavigationIndex.value),
-            appBar: AppBar(),
-            body: Column(
-              children: [
-                TextField(
-                  style: const TextStyle(
-                    decoration: TextDecoration.none,
-                  ),
-                  decoration: InputDecoration(
-                    prefixText: "Title: ",
-                    prefixStyle: const TextStyle(color: Colors.green),
-                    counterText:
-                        detailsController.titleTextCounter.value.toString(),
-                  ),
+    return Obx(
+      () => Scaffold(
+        backgroundColor:
+            _getBackgroundColor(detailsController.bottomNavigationIndex.value),
+        appBar: AppBar(
+            leading: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () {
+                  Get.back();
+                })),
+        body: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.all(8),
+              child: TextField(
+                enableInteractiveSelection: true,
+                buildCounter: (context,
+                    {required currentLength, required isFocused, maxLength}) {
+                  return Text(
+                    "${currentLength.toInt()}",
+                    style: const TextStyle(decoration: TextDecoration.none),
+                  );
+                },
+                style: const TextStyle(
+                  decoration: TextDecoration.none,
                 ),
-                Expanded(
-                  child: TextField(
-                      expands: true,
-                      maxLines: null,
-                      minLines: null,
-                      decoration: InputDecoration(
-                          counterText: detailsController.detailsTextCounter.value
-                              .toString()),
-                      style: TextStyle(decoration: TextDecoration.none)),
-                )
-              ],
+                decoration: const InputDecoration(
+                    isDense: true,
+                    prefixText: "Title: ",
+                    prefixStyle: TextStyle(color: Colors.green),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Colors.teal,
+                            width: 3,
+                            style: BorderStyle.solid))),
+              ),
             ),
-            bottomNavigationBar: BottomNavigationBar(
-              type: BottomNavigationBarType.shifting,
-              selectedItemColor: AppColors.colorAccent,
-              elevation: 24,
-              showSelectedLabels: false,
-              showUnselectedLabels: false,
-              currentIndex: detailsController.bottomNavigationIndex.value,
-              items: const [
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.circle, color: Colors.white),
-                    activeIcon: Icon(
-                      Icons.circle,
-                      color: Colors.white,
-                    ),
-                    label: "white",
-                    backgroundColor: Colors.white),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.circle, color: Colors.grey),
-                    activeIcon: Icon(Icons.circle, color: Colors.grey),
-                    label: "grey",
-                    backgroundColor: Colors.grey),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.circle, color: Colors.blue),
-                    activeIcon: Icon(Icons.circle, color: Colors.blue),
-                    label: "blue",
-                    backgroundColor: Colors.blue),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.circle, color: Colors.lime),
-                    activeIcon: Icon(Icons.circle, color: Colors.lime),
-                    label: "lime",
-                    backgroundColor: Colors.lime),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.circle, color: Colors.amber),
-                    activeIcon: Icon(Icons.circle, color: Colors.amber),
-                    label: "amber",
-                    backgroundColor: Colors.amber),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.circle, color: Colors.deepOrangeAccent),
-                    activeIcon:
-                        Icon(Icons.circle, color: Colors.deepOrangeAccent),
-                    label: "orange",
-                    backgroundColor: Colors.deepOrangeAccent),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.circle, color: Colors.deepPurple),
-                    activeIcon: Icon(Icons.circle, color: Colors.deepPurple),
-                    label: "purple",
-                    backgroundColor: Colors.deepPurple)
-              ],
-              onTap: (int index) {
-                detailsController.bottomNavigationIndex.value = index;
-                _getBackgroundColor(index);
-              },
-            ))
-        // child: const TextField(
-        //   showCursor: true,
-        //   autocorrect: true,
-        //   cursorColor: Colors.blue,
-        //
-        // ),
-        );
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.all(8),
+                child: TextField(
+                    textInputAction: TextInputAction.done,
+                    textAlignVertical: TextAlignVertical.top,
+                    enableInteractiveSelection: true,
+                    expands: true,
+                    maxLines: null,
+                    minLines: null,
+                    buildCounter: (context,
+                        {required currentLength,
+                        required isFocused,
+                        maxLength}) {
+                      return Text(
+                        "${currentLength.toInt()}",
+                        style: const TextStyle(decoration: TextDecoration.none),
+                      );
+                    },
+                    decoration: const InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.teal,
+                                width: 3,
+                                style: BorderStyle.solid))),
+                    style: const TextStyle(decoration: TextDecoration.none)),
+              ),
+            )
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.shifting,
+          selectedItemColor: AppColors.colorAccent,
+          elevation: 24,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          currentIndex: detailsController.bottomNavigationIndex.value,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.circle, color: Colors.white),
+                activeIcon: Icon(
+                  Icons.circle,
+                  color: Colors.white,
+                ),
+                label: "white",
+                backgroundColor: Colors.white),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.circle, color: Colors.grey),
+                activeIcon: Icon(Icons.circle, color: Colors.grey),
+                label: "grey",
+                backgroundColor: Colors.grey),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.circle, color: Colors.blue),
+                activeIcon: Icon(Icons.circle, color: Colors.blue),
+                label: "blue",
+                backgroundColor: Colors.blue),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.circle, color: Colors.lime),
+                activeIcon: Icon(Icons.circle, color: Colors.lime),
+                label: "lime",
+                backgroundColor: Colors.lime),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.circle, color: Colors.amber),
+                activeIcon: Icon(Icons.circle, color: Colors.amber),
+                label: "amber",
+                backgroundColor: Colors.amber),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.circle, color: Colors.deepOrangeAccent),
+                activeIcon: Icon(Icons.circle, color: Colors.deepOrangeAccent),
+                label: "orange",
+                backgroundColor: Colors.deepOrangeAccent),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.circle, color: Colors.deepPurple),
+                activeIcon: Icon(Icons.circle, color: Colors.deepPurple),
+                label: "purple",
+                backgroundColor: Colors.deepPurple)
+          ],
+          onTap: (int index) {
+            detailsController.bottomNavigationIndex.value = index;
+            _getBackgroundColor(index);
+          },
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.miniStartFloat,
+        floatingActionButton: FloatingActionButton(focusElevation: 16,child: Icon(Icons.add),mini: true,
+          onPressed: () {},
+        ),
+      ),
+    );
   }
 
   Color _getBackgroundColor(int index) {
